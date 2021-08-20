b3e38582d90161bdd5d2177d0b77033a23b3d890
Luca Agra <luca.agra@hotmail.com>
2021-08-20 16:34:48 -0300

    test: Adicionando testes e melohrando css

--- src/templates/Main/styles.ts
+++ src/templates/Main/styles.ts
@@ -6,107 +6,98 @@ export const Wrapper = styled.main`
     background-color: ${theme.colors.black};
     width: 100%;
     height: 100%;
-    padding: 3rem;
+    padding: ${theme.spacings.medium};
     display: flex;
     justify-content: center;
     align-items: center;
-    color: #fff;
-  `}
-
-  ${media.lessThan("medium")`
-    border: 1px solid red;
-
-    flex-direction: column;
-
+    color: ${theme.colors.white};
   `}
 `;
 
 export const Logo = styled.img`
   width: 10rem;
-  margin-bottom: 2rem;
 `;
 
 export const TitleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
 
-export const Title = styled.h1`
-  font-size: 6rem;
+    .login--title {
+      font-size: calc(${theme.font.sizes.large} * 2);
+    }
+  `}
 `;
 
 export const Description = styled.p`
-  font-size: 2.8rem;
-  font-weight: 400;
-  line-height: 4.3rem;
-  max-width: 40rem;
-  text-align: left;
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.large};
+    font-weight: ${theme.font.weight.thin};
+    line-height: calc(${theme.font.sizes.large} * 1.2);
+    max-width: 40rem;
+  `}
 `;
 
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 1;
-  padding-left: 10rem;
 
-  ${media.lessThan("medium")`
+  ${media.lessThan("large")`
     justify-content: center;
-    padding-left: 2rem;
+    align-items: center;
   `}
 `;
 
 export const IlustrationWrapper = styled.div`
-  flex: 1;
-
-  ${media.lessThan("medium")`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `}
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Illustration = styled.img`
-  margin-top: 3rem;
   width: min(30rem, 100%);
+
+  ${media.lessThan("large")`
+    display: none;
+  `}
 `;
 
 export const WaveIllustration = styled.img`
   position: absolute;
   bottom: 0;
+  left: 0;
   width: 100vw;
 `;
 
 export const Button = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-  width: 40rem;
-  border-radius: 8px;
-  padding: 1rem;
-  font-size: 1.8rem;
-  font-weight: 500;
-  cursor: pointer;
-  transition: filter 0.2s;
-  margin-top: 2rem;
-
   ${({ theme }) => css`
-    border: 2px solid ${theme.colors.white};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    width: 40rem;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xsmall};
+    font-size: ${theme.font.sizes.small};
+    font-weight: ${theme.font.weight.normal};
+    cursor: pointer;
+    transition: filter 0.2s;
+    margin-top: ${theme.spacings.small};
+    border: 0.2rem solid ${theme.colors.white};
     color: ${theme.colors.white};
     background-color: ${theme.colors.black};
-  `}
 
-  & > img {
-    width: 2.8rem;
-    height: 2.8rem;
-  }
+    .login--google-logo {
+      width: 2.8rem;
+      height: 2.8rem;
+    }
 
-  &:hover {
-    filter: brightness(0.8);
-  }
+    &:hover {
+      filter: brightness(0.8);
+    }
 
-  ${media.lessThan("medium")`
-      width: 25rem;
-
+    ${media.lessThan("medium")`
+      width: 100%;
+   `}
   `}
 `;
